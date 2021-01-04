4bac3303d6c6594982180fed4cd7bcb157168d15
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2021-01-04 11:03:15 +0100

    création du test distanceTo()

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -6,9 +6,19 @@ import org.junit.Test;
 
 public class CityTest {
 
+	/**
+	 * GIVEN distanceTo
+	 * WHEN ville1(1,1) et ville(1,2)
+	 * THEN 1
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDistanceTo_ville1_ville2_1() {
+		City ville1 = new City(1,1);
+		City ville2 = new City(1, 2);
+
+		double expected =1;
+		double actual =ville1.distanceTo(ville2);
+		assertEquals(expected, actual,0);
 	}
 
 }
