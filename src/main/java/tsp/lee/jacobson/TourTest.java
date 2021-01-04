969881e65d219f8751868059bc9a7ba3e163073b
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2021-01-04 12:14:03 +0100

    [Test] Added tour other tests.

--- src/main/java/tsp/lee/jacobson/TourTest.java
+++ src/main/java/tsp/lee/jacobson/TourTest.java
@@ -11,12 +11,34 @@ class TourTest {
      * When get distance, default 0
      * Then distance == 0
      */
-    void getDistance() {
+    void getDistance_isSame() {
         Tour tourA = new Tour();
 
         int actual = tourA.getDistance();
         int expected = 0;
         assertEquals(actual, expected);
 
+    }
+
+    @Test
+    /**
+     * Given getDistance
+     * When only one city
+     * Then distance is 0
+     */
+    public void getDistance_OnlyOneCity_0() {
+        City city1 = new City(5,4);
+
+        // add city to possible individual list
+        TourManager.addCity(city1);
+        // since one individual is in the TourManager.destinationCities
+        // the constructor will prepare an ArrayList with one element
+        Tour tour = new Tour();
+        tour.setCity(0, city1);
+
+        double actual = tour.getDistance();
+        double expected = 0;
+
+        assertEquals(expected, actual, 0);
     }
 }
