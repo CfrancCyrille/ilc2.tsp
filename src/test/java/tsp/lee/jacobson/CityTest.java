56367fbce6cd29f4c8634116bfd856d18e8ee85f
youssef-t <yousseftaleb1999@gmail.com>
2021-01-04 12:13:17 +0100

    Youssef TALEB - tests added to City and Tour classes

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -9,29 +9,36 @@ public class CityTest {
 	City city0 = new City(0,0);
 	City city_pos = new City(5,4);
 	City city_neg = new City(-5, -4);
+	//epsilon used to specify error margin
+	double epsilon = 0.001;
 
 	@Test
 	/**
-	 * GIVEN_WHEN_THEN
-	 * distance0 = sqrt(x²+y²)
+	 * GIVEN distanceTO
+	 * WHEN distance between city0 and other cities
+	 * THEN distance = sqrt(x²+y²)
 	 */
 	public void distanceTo_city0_distance0() {
 		double actual_distance = city_pos.distanceTo(city0);
 		double expected_distance = Math.sqrt(city_pos.getX()*city_pos.getX() + city_pos.getY()*city_pos.getY());
-		//epsilon used to specify error margin
-		double epsilon = 0.001;
+
 		assertEquals(expected_distance, actual_distance, epsilon);
 	}
 
 	@Test
-	//distance to the same city equals 0
+	/**
+	 * GIVEN distanceTO
+	 * WHEN distance between the same city
+	 * THEN distance = 0
+	 */
 	public void distanceTo_sameCity_0(){
 		double actual_distance1 = city_pos.distanceTo(city_pos);
 		double actual_distance2 = city_neg.distanceTo(city_neg);
 		double expected_distance = 0.0;
 
-		assertEquals(expected_distance, actual_distance1, 0.001);
-		assertEquals(expected_distance, actual_distance2, 0.001);
+		assertEquals(expected_distance, actual_distance1, epsilon);
+		assertEquals(expected_distance, actual_distance2, epsilon);
 	}
+
 
 }
--- src/test/java/tsp/lee/jacobson/TourTest.java
+++ src/test/java/tsp/lee/jacobson/TourTest.java
@@ -6,9 +6,65 @@ import org.junit.Test;
 
 public class TourTest {
 
+	City city1 = new City(11, 10);
+	City city2 = new City(8, 6);
+	City city3 = new City(5,2);
+	double epsilon = 0.001;
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	/**
+	 * GIVEN getDistance
+	 * WHEN 0 city
+	 * Then distance = 0
+	 */
+	public void getDistance_0city_distanceNull(){
+		Tour tour = new Tour();
+		tour.setCity(0, city1);
+		double actual_distance = tour.getDistance();
+		double expected_distance = 0;
+
+		assertEquals(expected_distance, actual_distance, epsilon);
+	}
+
+	@Test
+	/**
+	 * GIVEN getDistance
+	 * WHEN 1 city
+	 * THEN distance = 0
+	 */
+	public void getDistance_1city_distanceNull(){
+		TourManager.addCity(city1);
+
+		Tour tour = new Tour();
+		tour.setCity(0, city1);
+
+		double actual_distance = tour.getDistance();
+		double expected_distance = 0;
+
+		assertEquals(expected_distance, actual_distance, epsilon);
+	}
+
+	@Test
+	/**
+	 * GIVEN getDistance
+	 * WHEN 3 cities
+	 * THEN all distances between 2 cities are added
+	 */
+	public void getDistance_3cities_sumDistances(){
+		//city1 is already added
+		TourManager.addCity(city2);
+		TourManager.addCity(city3);
+
+		Tour tour = new Tour();
+		tour.setCity(0, city1);
+		tour.setCity(1, city2);
+		tour.setCity(2, city3);
+
+		double actual_distance = tour.getDistance();
+		//distance betweeen each city
+		double expected_distance = 5 + 5 + 10;
+
+		assertEquals(expected_distance, actual_distance, epsilon);
 	}
 
 }
