3648899974c8626ed7991c758c19e3cbd3f37fba
charbel-elhajj <charbelelhajj99@hotmail.com>
2021-01-04 11:01:21 +0100

    Add tests to City class

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -7,8 +7,71 @@ import org.junit.Test;
 public class CityTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	/**
+	 * Given distanceTo
+	 * When two cities with same coordinates
+	 * Then distance is 0
+	 */
+	public void distanceTo_SameCoordinates_0() {
+		City c1 = new City(1,1);
+		City c2 = new City(1,1);
+		
+		double actual = c1.distanceTo(c2);
+		double expected = 0;
+		
+		assertEquals(actual, expected,0);
+		
+	}
+	
+	@Test
+	/**
+	 * Given distanceTo
+	 * When two cities with horizentally aligned
+	 * Then distance is difference between absciss
+	 */
+	public void distanceTo_HorizentalyAlligned_DiffAbscices() {
+		City c1 = new City(1,1);
+		City c2 = new City(2,1);
+		
+		double actual = c1.distanceTo(c2);
+		double expected = 1;
+		
+		assertEquals(actual, expected,0);
+		
+	}
+	
+	@Test
+	/**
+	 * Given distanceTo
+	 * When two cities with Vertically aligned
+	 * Then distance is difference between ordinates
+	 */
+	public void distanceTo_VerticallyAlligned_DiffOrdinates() {
+		City c1 = new City(1,2);
+		City c2 = new City(1,1);
+		
+		double actual = c1.distanceTo(c2);
+		double expected = 1;
+		
+		assertEquals(actual, expected,0);
+		
+	}
+	
+	@Test
+	/**
+	 * Given distanceTo
+	 * When two cities with Diff absciss and ordinate
+	 * Then valu2 of the segment joining them
+	 */
+	public void distanceTo_DiffAbscissAndOrdinate_ValueOfSegmentJoiningThem() {
+		City c1 = new City(2,2);
+		City c2 = new City(1,1);
+		
+		double actual = c1.distanceTo(c2);
+		double expected = Math.sqrt(2);
+		
+		assertEquals(actual, expected,0);
+		
 	}
 
 }
