05736cfc33adc6428faada2da14a0b5f226bea0a
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2021-01-04 12:15:42 +0100

    [Test] Added GA other tests.

--- src/main/java/tsp/lee/jacobson/GATest.java
+++ src/main/java/tsp/lee/jacobson/GATest.java
@@ -12,7 +12,7 @@ class GATest {
      * When evolve population
      * Then population is evolved if fitness is changed
      */
-    void evolvePopulation() {
+    void evolvePopulation_isEvoluted() {
         // PopulationSize for 100 and inisialise is true
         Population pop = new Population(100, true);
 
